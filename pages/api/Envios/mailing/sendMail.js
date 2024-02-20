f01f9cd382c004aed969be627cf1ac8cddb40f08
Ignacio Arzaut <ignacio.arzaut@themobian.com>
2024-02-19 23:18:49 -0300

    asd

--- pages/api/Envios/mailing/sendMail.js
+++ pages/api/Envios/mailing/sendMail.js
@@ -1,132 +1,40 @@
-/* import nodemailer from 'nodemailer'; */
 
-// Configura el transporte
+import nodemailer from 'nodemailer';
 
-// Manejador de la ruta API
-/* export default async function handler(req, res) { */
-    /* const transporter = nodemailer.createTransport({
-        service: 'Outlook',
-        auth: {
-            user: 'ghersinichmatias@outlook.com', // Cambia esto por tu dirección de correo electrónico
-            pass: 'M4tistartv' // Cambia esto por tu contraseña
-        }
-    });  */   
-    
-    /* if (req.method === 'POST') {
-        const { text } = req.body;
-        
-        try {	   
-            await nodemailer
-              .createTransport({
-                service: 'Outlook',
-                auth: {
-                  user: 'ghersinichmatias@outlook.com',
-                  pass: 'M4tistartv'
-                },
-              })
-              .sendMail({
-                from: 'ghersinichmatias@outlook.com', // Dirección de correo electrónico del remitente
-                to: 'ghersinichmatias@outlook.com', // Dirección de correo electrónico del destinatario
-                subject: 'Nueva Posible Compra', // Asunto del correo electrónico
-                text: text
-              })
-            console.log('Email sent to ' + email)
-            res.status(200).json({ message: 'Correo electrónico enviado correctamente.' });
-        } catch (e) {
-          console.error(e)
-        } */
-        /* // Configura los detalles del correo electrónico
-        const mailOptions = {
-            from: 'ghersinichmatias@outlook.com', // Dirección de correo electrónico del remitente
-            to: 'ghersinichmatias@outlook.com', // Dirección de correo electrónico del destinatario
-            subject: 'Nueva Posible Compra', // Asunto del correo electrónico
-            text: text // Cuerpo del correo electrónico
-        };
-        
-        try {
-            // Verifica la conexión con el servidor de correo electrónico
-            await new Promise((resolve, reject) => {
-                // verify connection configuration
-                transporter.verify(function (error, success) {
-                    if (error) {
-                        console.log(error);
-                        reject(error);
-                    } else {
-                        console.log("Server is ready to take our messages");
-                        resolve(success);
-                    }
-                });
-            });
-            
-            // Envía el correo electrónico
-            await new Promise((resolve, reject) => {
-                // send mail
-                transporter.sendMail(mailOptions, (err, info) => {
-                    if (err) {
-                        console.error(err);
-                        reject(err);
-                    } else {
-                        console.log(info);
-                        resolve(info);
-                    }
-                });
-            });
-            res.status(200).json({ message: 'Correo electrónico enviado correctamente.' });
-        } catch (error) {
-            console.error('Error al enviar el correo electrónico:', error);
-            res.status(500).json({ message: 'Error al enviar el correo electrónico.' });
-        }
-    } else {
-        res.status(405).json({ message: 'Método no permitido.' });
-    } */
+async function handler(text) {
+    try {
+        const transporter = nodemailer.createTransport({
+            service: "Gmail",
+            host: "smtp.gmail.com",
+            port: 465,
+            secure: true,
+            auth: {
+                user: 'arzautignacio@gmail.com',
+                pass: 'lyfgticcayyzfplr'
+            }
+        });
 
-    /* import { handler } from '../../../../db/Controladores/Mailing/sendMail';  */
-    import nodemailer from 'nodemailer';
+        await transporter.sendMail({
+            from: 'arzautignacio@gmail.com',
+            to: 'arzautignacio+1@gmail.com',
+            subject: 'Nueva Posible Compra',
+            text: text
+        });
 
-    async function handler(text) {
-        try {
-            const transporter = nodemailer.createTransport({
-                host: "smtp-mail.outlook.com",
-                port: 587,
-                secure: false,
-                auth: {
-                    user: 'ghersinichmatias@outlook.com',
-                    pass: 'M4tistartv'
-                }
-            });
+        /* await transporter.sendMail({
+            from: 'ghersinichmatias@outlook.com',
+            to: 'ghersinichmatias@outlook.com',
+            subject: 'Nueva Posible Compra',
+            text: text
+        }); */
 
-            await new Promise((resolve, reject) => {
-                // send mail
-                transporter.sendMail({
-                    from: 'ghersinichmatias@outlook.com',
-                    to: 'ghersinichmatias@outlook.com',
-                    subject: 'Nueva Posible Compra',
-                    text: text
-                }, (err, info) => {
-                    if (err) {
-                        console.error(err);
-                        reject(err);
-                    } else {
-                        console.log(info);
-                        resolve(info);
-                    }
-                });
-            });
-    
-            /* await transporter.sendMail({
-                from: 'ghersinichmatias@outlook.com',
-                to: 'ghersinichmatias@outlook.com',
-                subject: 'Nueva Posible Compra',
-                text: text
-            }); */
-    
-            console.log('Correo electrónico enviado correctamente.');
-            return true; // Indica que el correo se envió correctamente
-        } catch (error) {
-            console.error('Error al enviar el correo electrónico:', error);
-            throw error; // Propaga el error para que sea manejado por el controlador
-        }
+        console.log('Correo electrónico enviado correctamente.');
+        return true; // Indica que el correo se envió correctamente
+    } catch (error) {
+        console.error('Error al enviar el correo electrónico:', error);
+        throw error; // Propaga el error para que sea manejado por el controlador
     }
+}
 
 export default async (req, res) => {
     if (req.method === 'POST') {
